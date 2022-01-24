390e53edc5d106d85ed671da6d7705bdb8aaaa70
gpiccin <gpiccin@outlook.com>
2022-01-24 11:22:10 -0300

    v.0.9

--- bombcrypto/GreenBarStrategy.py
+++ bombcrypto/GreenBarStrategy.py
@@ -32,10 +32,16 @@ class GreenBarStrategy:
                                                   [self._image_processor.image], seconds_waiting=2)
 
         if execution_result == MethodExecutor.SUCCESS:
-            MethodExecutor.execute(self.return_to_work,
-                                                  [self._image_processor.image],
-                                                  self._image_processor.is_playing,
-                                                  [self._image_processor.image])
+            execution_result = MethodExecutor.execute(self.return_to_work,
+                                                      [self._image_processor.image],
+                                                      self._image_processor.is_playing,
+                                                      [self._image_processor.image])
+
+            if execution_result == MethodExecutor.FAIL:
+                MethodExecutor.execute(self.go_to_back,
+                                       [image],
+                                       self._image_processor.is_treasure_hunt_screen,
+                                       [self._image_processor.image])
 
         return True
 
@@ -50,3 +56,9 @@ class GreenBarStrategy:
 
         if close:
             ActionExecutor.click(close.first_point())
+
+    def go_to_back(self, image):
+        back = self._image_processor.back(image)
+
+        if back:
+            ActionExecutor.click(back.first_point())
